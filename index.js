c7b0cbe3354294949bd95b0e16d1deb818d014bb
HuyLD <Huy.LuuDuc@nttdata.com>
2018-05-28 17:38:35 +0700

    feat(get-repo-info): request repo info and filter results

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ require('./tasks/get-input-data.js')
   .then(configData => {
     return require('./tasks/get-repository-list.js').requestRepositoriesInfo(configData);
   }).then(repositories => {
+    const jsonStr = JSON.stringify(repositories, null, 4);
     // Write to file to make it easier to check (use in developing phase only)
-    fs.writeFile('response.json', repositories, (err) => {
+    fs.writeFile('response.json', jsonStr, (err) => {
       if (err) {
         console.error('Failed to write to file.', err);
       }
--- tasks/get-repository-list.js
+++ tasks/get-repository-list.js
@@ -23,23 +23,83 @@ const interestingLinks = [
  * @param {any} configData
  */
 function requestRepositoriesInfo(configData) {
+  console.info('Requesting repository list...');
   return new Promise((resolve, reject) => {
-  request
-    .get(configData.repositoriesURL, {
-      auth: {
-        user: configData.username,
-        pass: configData.password,
-      }
-    })
-    .on('response', (res) => {
-      res.body = "";
-      res.on('data', function (chunk) {
-        res.body += chunk;
-      });
+    request
+      .get(configData.repositoriesURL, {
+        auth: {
+          user: configData.username,
+          pass: configData.password,
+        }
+      })
+      .on('response', (res) => {
+        res.body = "";
+        res.on('data', function (chunk) {
+          res.body += chunk;
+        });
 
-      res.on('end', function () {
+        res.on('end', function () {
+          const jsonObj = JSON.parse(res.body);
+          const nextURL = jsonObj.next;
+
+          if (nextURL) {
+            getRepoListRecursively(nextURL, configData).then(result => {
+              console.info('Finish requesting repository list.');
+              resolve(result);
+            });
+          } else {
+            console.info('Finish requesting repository list.');
+            resolve(jsonObj);
+          }
+        });
       });
-    });
+  });
+}
+
+/**
+ * Since the repositories in the response is paginated,
+ * this method recursively calls itself to get the repositories in the next page.
+ * After it reaches the last page, it returns a list of repositories in that page.
+ *
+ * @param {any} requestURL
+ * @param {any} configData
+ */
+function getRepoListRecursively(requestURL, configData) {
+  return new Promise((resolve, reject) => {
+    request
+      .get(requestURL, {
+        auth: {
+          user: configData.username,
+          pass: configData.password,
+        }
+      })
+      .on('response', (res) => {
+        res.body = "";
+        res.on('data', function (chunk) {
+          res.body += chunk;
+        });
+
+        res.on('end', function () {
+          const jsonObj = JSON.parse(res.body);
+          const repoList = jsonObj.values;
+
+          // Filter repository objects to get only interesting properties
+          const filteredRepoList = repoList
+            .map(el => filterInterestingProperties(el, interestingProperties));
+
+          const nextURL = jsonObj.next;
+          if (nextURL) {
+            getRepoListRecursively(nextURL, configData)
+              .then(returnedRepoList => {
+                // Append the returned repoList to the current repoList
+                resolve([...returnedRepoList, ...filteredRepoList]);
+              });
+          } else {
+            // Resolve the current repoList
+            resolve(filteredRepoList);
+          }
+        });
+      });
   });
 }
 
